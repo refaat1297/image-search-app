c30385df0b9bc6ca5fdaf5b8971e699033d182d5
Ahmed Refaat <43079675+refaat1297@users.noreply.github.com>
2020-02-01 20:09:55 +0200

    Add files via upload

---
+++ precache-manifest.0255dcf078b00ebda5b9ebc0c7dab7e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8135f0455180bbc8047e6e99a0281e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751f8d9b34a097b02840",
+    "url": "/static/css/2.1087be86.chunk.css"
+  },
+  {
+    "revision": "b63d2cd4c0a153256bee",
+    "url": "/static/css/main.26aae016.chunk.css"
+  },
+  {
+    "revision": "751f8d9b34a097b02840",
+    "url": "/static/js/2.d9f28260.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d9f28260.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b63d2cd4c0a153256bee",
+    "url": "/static/js/main.b69d5626.chunk.js"
+  },
+  {
+    "revision": "e0cca531239b9b532c96",
+    "url": "/static/js/runtime-main.fc144f28.js"
+  }
+]);
